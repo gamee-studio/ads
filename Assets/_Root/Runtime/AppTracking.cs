06a796154ced56ea6e9ce3e5dcc42fff4d902454
yenmoc <phongsoyenmoc.diep@gmail.com>
2022-04-28 16:17:36 +0700

    *WIP add app tracking firebase and adjust as default

---
+++ Assets/_Root/Runtime/AppTracking.cs
@@ -0,0 +1,46 @@
+#if ADS_FIREBASE_TRACKING
+using Firebase.Analytics;
+
+namespace Pancake.Monetization
+{
+    internal static class AppTracking
+    {
+#if PANCAKE_MAX_ENABLE
+        internal static void TrackingRevenue(MaxSdkBase.AdInfo adInfo)
+        {
+            // Log an event with ad value parameters
+            Parameter[] LTVParameters =
+            {
+                // Log ad value in micros.
+                new Parameter("valuemicros", adInfo.Revenue),
+                // These values below won’t be used in ROASrecipe.
+                // But log for purposes of debugging and futurereference.
+                new Parameter("currency", "USD"), new Parameter("precision", 1), new Parameter("adunitid", adInfo.AdUnitIdentifier),
+                new Parameter("network", adInfo.NetworkName)
+            };
+
+            FirebaseAnalytics.LogEvent("paid_ad_impression", LTVParameters);
+        }
+#endif
+
+#if PANCAKE_ADMOB_ENABLE
+        internal static void TrackingRevenue(GoogleMobileAds.Api.AdValueEventArgs e, string unitId)
+        {
+            // Log an event with ad value parameters
+            Parameter[] LTVParameters =
+            {
+                // Log ad value in micros.
+                new Parameter("valuemicros", e.AdValue.Value),
+                // These values below won’t be used in ROASrecipe.
+                // But log for purposes of debugging and futurereference.
+                new Parameter("currency", e.AdValue.CurrencyCode), new Parameter("precision", (int) e.AdValue.Precision), new Parameter("adunitid", unitId),
+                new Parameter("network", "admob")
+            };
+
+            FirebaseAnalytics.LogEvent("paid_ad_impression", LTVParameters);
+        }
+#endif
+    }
+}
+
+#endif
